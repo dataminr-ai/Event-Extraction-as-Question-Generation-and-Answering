24791fbbb850e2195cda7a971af446c03cd26f31
Di Lu <dlu@dataminr.com>
2023-07-20 07:44:34 -0600

    update code for T5 model version

--- train_qa_t5.sh
+++ train_qa_t5.sh
@@ -1,6 +1,8 @@
 python src/train_argument_extraction.py  --lr 20e-5 \
                               --batch_size 2 --gpu 0 \
                               --gradient_accumulation_steps 32 \
-                              --opt adafactor --multi_arg \
-                              --transformer_name t5-large \
-                              --qg_model_type t5
+                              --opt 'adafactor' --multi_arg \
+                              --transformer_name 't5-large' \
+                              --qg_model_type 't5' \
+                              --qg_model_path './model_checkpoint/qg_model_t5' \
+                              --models_dir './model_checkpoint/eae_model_t5'
--- train_qg_t5.sh
+++ train_qg_t5.sh
@@ -1,1 +1,5 @@
-python src/train_question_generation.py --lr 10e-5 --batch_size 2 --gpu 0 --opt 'adafactor'  --transformer_name 't5-large'
+python src/train_question_generation.py --lr 10e-5 \
+                                        --batch_size 2 --gpu 0 \
+                                        --opt 'adafactor'  \
+                                        --transformer_name 't5-large' \
+                                        --models_dir  './model_checkpoint/qg_model_t5'
